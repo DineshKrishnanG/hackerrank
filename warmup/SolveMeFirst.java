5d59e5af38eecfe7621bbe19e2612a075af93b27
DineshKrishnanG <dinesh.gk.krishnan@gmail.com>
2016-02-03 15:44:06 +0530

    Create SolveMeFirst.java
    sum of 2 integers

---
+++ warmup/SolveMeFirst.java
@@ -0,0 +1,31 @@
+/*
+Let's start simple. 
+Can you complete the function solveMeFirst to return the sum of two integers passed as input parameters? 
+You can pick your favorite programming language.
+*/
+
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class SolveMeFirst {
+
+
+    static int solveMeFirst(int a, int b) {
+        return a+b;
+   }
+
+   
+ public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int _a;
+        _a = in.nextInt();
+        int _b;
+        _b = in.nextInt();
+        int sum;
+        sum = solveMeFirst(_a, _b);
+        System.out.println(sum);
+   }
+}
